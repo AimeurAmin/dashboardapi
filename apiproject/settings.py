2264724303a336007f8dbd8a9a06e03d456cd6c6
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-08-25 16:48:47 +0100

    settings update

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {  
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'dashboardOoredoo',  
-        'USER': 'root',  
-        'PASSWORD': '',  
+        'USER': 'amin',  
+        'PASSWORD': 'password',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
